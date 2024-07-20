711bc0cce43fd23fe11c956179ef759b991e5328
Kunniii <57139436+Kunniii@users.noreply.github.com>
2024-07-20 18:22:23 +0700

    validate input and infinit loop

--- golang-functions-methods-interfaces/mod-3/main.go
+++ golang-functions-methods-interfaces/mod-3/main.go
@@ -41,15 +41,24 @@ func printResult(a *Animal, info string) {
 	}
 }
 
-func createInputString() func(prompt string) (string, string) {
+func createInputValidator() func(prompt string) (string, string, bool) {
 	reader := bufio.NewReader(os.Stdin)
-	return func(prompt string) (string, string) {
+	return func(prompt string) (string, string, bool) {
 		fmt.Print(prompt)
 		inp, _ := reader.ReadString('\n')
 		inp = strings.TrimSuffix(inp, "\n")
-		a := strings.Split(inp, " ")[0]
-		b := strings.Split(inp, " ")[1]
-		return strings.ToLower(a), strings.ToLower(b)
+		if len(strings.Split(inp, " ")) < 2 {
+			return "", "", false
+		}
+		a := strings.ToLower(strings.Split(inp, " ")[0])
+		b := strings.ToLower(strings.Split(inp, " ")[1])
+		if a != "cow" && a != "bird" && a != "snake" {
+			return "", "", false
+		}
+		if b != "eat" && b != "move" && b != "speak" {
+			return "", "", false
+		}
+		return a, b, true
 	}
 }
 
@@ -60,18 +69,22 @@ func main() {
 
 	fmt.Println("Get information of `cow`, 'bird`, and `snake`")
 	fmt.Println("The information are `eat`, 'move`, and `speak`")
+	input := createInputValidator()
 
-	input := createInputString()
-	name, info := input("> ")
-
-	if name == "cow" {
-		printResult(cow, info)
+	for {
+		if name, info, ok := input("> "); ok {
+			if name == "cow" {
+				printResult(cow, info)
+			}
+			if name == "snake" {
+				printResult(snake, info)
+			}
+			if name == "bird" {
+				printResult(bird, info)
+			}
+		} else {
+			fmt.Println("Get information of `cow`, 'bird`, and `snake`")
+			fmt.Println("The information are `eat`, 'move`, and `speak`")
+		}
 	}
-	if name == "snake" {
-		printResult(snake, info)
-	}
-	if name == "bird" {
-		printResult(bird, info)
-	}
-
 }
